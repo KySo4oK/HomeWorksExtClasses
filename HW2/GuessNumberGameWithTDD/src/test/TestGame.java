a7f7e512fb49a21e24fa158b4e7cdb52ed1ea00a
KySo4oK <prostorostuk08@gmail.com>
2020-02-07 14:54:07 +0200

    add new test - make him failed

--- HW2/GuessNumberGameWithTDD/src/test/TestGame.java
+++ HW2/GuessNumberGameWithTDD/src/test/TestGame.java
@@ -2,14 +2,29 @@ package test;
 
 import game.Controller;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
 
 public class TestGame {
+    private static Controller controller;
+
+    public void initController() {
+        controller = new Controller();
+    }
+
     @Test
-    public void testComparing_1_and_2(){
-        Controller controller = new Controller();
-        String result = controller.compareNumbers(1,2);
+    public void testComparing_1_and_2() {
+        initController();
+        String result = controller.compareNumbers(1, 2);
         Assert.assertEquals("greater", result);
+    }
+
+    @Test
+    public void testComparing_2_and_1() {
+        initController();
+        String result = controller.compareNumbers(2, 1);
+        Assert.assertEquals("lesser", result);
     }
 
 }
