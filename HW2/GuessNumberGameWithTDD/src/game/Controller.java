156ce348826de294bad5e2d5b26ab46a9604584c
KySo4oK <prostorostuk08@gmail.com>
2020-02-07 15:58:40 +0200

    add a couple test for statistic

--- HW2/GuessNumberGameWithTDD/src/game/Controller.java
+++ HW2/GuessNumberGameWithTDD/src/game/Controller.java
@@ -1,11 +1,33 @@
 package game;
 
+import java.util.ArrayList;
+
 public class Controller {
+    private int numberOfAttempts;
+    private ArrayList<String> resultsOfAttempts = new ArrayList<>();
     public String compareNumbers(int firstNumber, int secondNumber) {
+        numberOfAttempts++;
         if (firstNumber < secondNumber) {
             return "greater";
         } else if(firstNumber > secondNumber) {
             return "lesser";
         } else return "equal";
     }
+
+    public int getRandomNumberBetween_100_0() {
+        return (int)Math.random()*101;
+    }
+
+    public int getNumberOfAttempts() {
+        return numberOfAttempts;
+    }
+
+    public String getStatistic() {
+        String result = "";
+        for(int i = 0; i < resultsOfAttempts.size(); i++){
+            result += resultsOfAttempts.get(i);
+        }
+        return result;
+    }
 }
+
--- HW2/GuessNumberGameWithTDD/src/test/TestGame.java
+++ HW2/GuessNumberGameWithTDD/src/test/TestGame.java
@@ -1,14 +1,12 @@
 package test;
 
 import game.Controller;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestGame {
     private static Controller controller;
+    private static int attempts;
 
     @BeforeClass
     public static void initController() {
@@ -30,8 +28,50 @@ public class TestGame {
         Assert.assertEquals("equal", compareNumbersForTest(1,1));
     }
 
+    @Test
+    public void testComparing_0_and_1() {
+        Assert.assertEquals("greater", compareNumbersForTest(0,1));
+    }
+
+    @Test
+    public void testRandomRangeLess(){
+        Assert.assertEquals("lesser", compareNumbersForTest(101,getRandomNumberFromController()));
+    }
+
+    @Test
+    public void testRandomRangeMore(){
+        Assert.assertEquals("greater", compareNumbersForTest(-1,getRandomNumberFromController()));
+    }
+
+    @Test
+    public void testGetNumberOfAttempts(){
+        testComparing_1_and_1();
+        Assert.assertEquals(controller.getNumberOfAttempts(), attempts);
+    }
+
+    @Test
+    public void testStatistic_0_and_1(){
+        testComparing_0_and_1();
+        Assert.assertEquals("greater than 0", controller.getStatistic());
+    }
+
+    @Test
+    public void testStatistic_101_and_0(){
+        testComparing_101_and_0();
+        Assert.assertEquals("lesser than 0", controller.getStatistic());
+    }
+
+    private void testComparing_101_and_0() {
+        Assert.assertEquals("lesser", compareNumbersForTest(101,0));
+    }
+
     public String compareNumbersForTest(int firstNumber, int secondNumber){
+        attempts++;
         return controller.compareNumbers(firstNumber, secondNumber);
+    }
+
+    public int getRandomNumberFromController(){
+        return controller.getRandomNumberBetween_100_0();
     }
 
 }
