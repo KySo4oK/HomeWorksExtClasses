1cbf202ead86c12a5b0c3d844045f2a244bfcd70
KySo4oK <prostorostuk08@gmail.com>
2020-02-09 19:30:34 +0200

    more refactor

--- HW3/RomanNumbers/src/main/java/groupId/ArabicToRomanNumberConverter.java
+++ HW3/RomanNumbers/src/main/java/groupId/ArabicToRomanNumberConverter.java
@@ -5,10 +5,20 @@ public class ArabicToRomanNumberConverter {
         boolean noNumber = arabicNumber == 0;
         if (noNumber)
             return getDefaultString();
+        return getRepeatingRomanNumber(arabicNumber);
+    }
+
+    private String getRepeatingRomanNumber(int arabicNumber) {
         String romanNumber = "";
+
+        while (arabicNumber >= 5) {
+            romanNumber += "V";
+            arabicNumber -= 5;
+        }
+
         while (arabicNumber >= 1) {
             romanNumber += "I";
-            arabicNumber--;
+            arabicNumber -= 1;
         }
         return romanNumber;
     }
--- HW3/RomanNumbers/src/test/java/groupId/WhenConvertArabicNumberToRoman.java
+++ HW3/RomanNumbers/src/test/java/groupId/WhenConvertArabicNumberToRoman.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 public class WhenConvertArabicNumberToRoman {
     @Test
-    public void Convert_0_To_EmptyString(){
+    public void Convert_0_To_EmptyString() {
         String romanNumber = convertToRoman(0);
         Assert.assertEquals("", romanNumber);
     }
@@ -16,20 +16,38 @@ public class WhenConvertArabicNumberToRoman {
     }
 
     @Test
-    public void Convert_1_To_I(){
+    public void Convert_1_To_I() {
         String romanNumber = convertToRoman(1);
         Assert.assertEquals("I", romanNumber);
     }
 
     @Test
-    public void Convert_2_To_II(){
+    public void Convert_2_To_II() {
         String romanNumber = convertToRoman(2);
         Assert.assertEquals("II", romanNumber);
     }
 
     @Test
-    public void Convert_3_To_III(){
+    public void Convert_3_To_III() {
         String romanNumber = convertToRoman(3);
         Assert.assertEquals("III", romanNumber);
+    }
+
+    @Test
+    public void Convert_5_To_V() {
+        String romanNumber = convertToRoman(5);
+        Assert.assertEquals("V", romanNumber);
+    }
+
+    @Test
+    public void Convert_6_To_VI() {
+        String romanNumber = convertToRoman(6);
+        Assert.assertEquals("VI", romanNumber);
+    }
+
+    @Test
+    public void Convert_8_To_VIII(){
+        String romanNumber = convertToRoman(8);
+        Assert.assertEquals("VIII", romanNumber);
     }
 }
