7d87d771f1e89503e17ec445e71539ad26a3b456
KySo4oK <prostorostuk08@gmail.com>
2020-02-09 17:40:14 +0200

    wrote third TDD cycle

--- HW3/RomanNumbers/src/main/java/groupId/ArabicToRomanNumberConverter.java
+++ HW3/RomanNumbers/src/main/java/groupId/ArabicToRomanNumberConverter.java
@@ -2,9 +2,15 @@ package groupId;
 
 public class ArabicToRomanNumberConverter {
     public String Convert(int arabicNumber) {
-        if(arabicNumber == 0)
-            return "";
-        else
-            return "I";
+        boolean noNumber = arabicNumber == 0;
+        if (noNumber)
+            return getDefaultString();
+        if (arabicNumber == 2)
+            return "II";
+        else return "I";
+    }
+
+    private String getDefaultString() {
+        return "";
     }
 }
--- HW3/RomanNumbers/src/test/java/groupId/WhenConvertArabicNumberToRoman.java
+++ HW3/RomanNumbers/src/test/java/groupId/WhenConvertArabicNumberToRoman.java
@@ -6,15 +6,24 @@ import org.junit.Test;
 public class WhenConvertArabicNumberToRoman {
     @Test
     public void Convert_0_To_EmptyString(){
-        ArabicToRomanNumberConverter converter = new ArabicToRomanNumberConverter();
-        String romanNumber = converter.Convert(0);
+        String romanNumber = convertToRoman(0);
         Assert.assertEquals("", romanNumber);
+    }
+
+    private String convertToRoman(int arabicNumber) {
+        ArabicToRomanNumberConverter converter = new ArabicToRomanNumberConverter();
+        return converter.Convert(arabicNumber);
     }
 
     @Test
     public void Convert_1_To_I(){
-        ArabicToRomanNumberConverter converter = new ArabicToRomanNumberConverter();
-        String romanNumber = converter.Convert(1);
+        String romanNumber = convertToRoman(1);
         Assert.assertEquals("I", romanNumber);
+    }
+
+    @Test
+    public void Convert_2_To_II(){
+        String romanNumber = convertToRoman(2);
+        Assert.assertEquals("II", romanNumber);
     }
 }
