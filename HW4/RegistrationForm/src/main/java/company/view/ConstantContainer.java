9ed6d3c90a12a9f7aa8085afb20d781b8e659cf3
KySo4oK <prostorostuk08@gmail.com>
2020-02-13 19:04:34 +0200

    remove unnecessary modification

--- HW4/RegistrationForm/src/main/java/company/view/ConstantContainer.java
+++ HW4/RegistrationForm/src/main/java/company/view/ConstantContainer.java
@@ -4,8 +4,8 @@ public interface ConstantContainer {
     /**
      * Constants for printing
      */
-    public static final String INPUT_STRING_DATA = "Input your - ";
-    public static final String LAST_NAME = "last name";
-    public static final String NICKNAME = " nickname";
-    public static final String WRONG_INPUT_STRING_DATA = "wrong input data, please try again";
+    String INPUT_STRING_DATA = "Input your - ";
+    String LAST_NAME = "last name";
+    String NICKNAME = " nickname";
+    String WRONG_INPUT_STRING_DATA = "wrong input data, please try again";
 }
