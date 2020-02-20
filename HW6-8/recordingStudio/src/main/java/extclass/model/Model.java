7715b24a71397d69c54cdd83e03658e9435697f9
KySo4oK <prostorostuk08@gmail.com>
2020-02-20 18:27:06 +0200

    implement all method in Model class

--- HW6-8/recordingStudio/src/main/java/extclass/model/Model.java
+++ HW6-8/recordingStudio/src/main/java/extclass/model/Model.java
@@ -1,42 +1,47 @@
 package extclass.model;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class Model {
-    private Sound[] sounds;
+    private ArrayList<Sound> sounds;
 
     public Model(int quantity) {
-        sounds = new Sound[quantity];
+        sounds = new ArrayList<>();
         for (int i = 0; i < quantity / 2; i++) {
-            sounds[i] = new Sound(60 + i * 10,
+            sounds.add(new Sound(60 + i * 10,
                     "writer" + i, "name" + i,
-                    Genre.values()[i % 4]);
+                    Genre.values()[i % 4]));
         }
         for (int i = quantity / 2; i < quantity; i++) {
-            sounds[i] = new Song(60 + i * 10,
+            sounds.add(new Song(60 + i * 10,
                     "writer" + i, "name" + i,
                     Genre.values()[i % 4],
-                    "Singer" + i);
+                    "Singer" + i));
         }
     }
 
 
     public ArrayList<Sound> getSoundsByRangeOfLength(int min, int max) {
         ArrayList<Sound> soundsByRange = new ArrayList<>();
-        for (int i = 0; i < sounds.length; i++) {
-            if ((sounds[i].getLength() >= min) && (sounds[i].getLength() <= max)) {
-                soundsByRange.add(sounds[i]);
+        for (int i = 0; i < sounds.size(); i++) {
+            if ((sounds.get(i).getLength() >= min) && (sounds.get(i).getLength() <= max)) {
+                soundsByRange.add(sounds.get(i));
             }
         }
         return soundsByRange;
     }
 
     public int getLength() {
-        return 0;
+        int length = 0;
+        for (int i = 0; i < sounds.size(); i++) {
+            length += sounds.get(i).getLength();
+        }
+        return length;
     }
 
     public void sortByGenre() {
-
+        sounds.sort(Comparator.comparing(Sound::getGenre));
     }
-
 }
