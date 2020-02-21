0797bb575399457332f3d1d42f27b061948a7e08
KySo4oK <prostorostuk08@gmail.com>
2020-02-21 17:17:38 +0200

    add work test

--- HW6-8/recordingStudio/src/test/TestModel.java
+++ HW6-8/recordingStudio/src/test/TestModel.java
@@ -1,10 +1,14 @@
 package test;
 
+import extclass.model.Model;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
 public class TestModel {
     @Test
     public void testLengthOfEmptyCollection(){
         Model model = new Model(0);
-        Assert.assertEqual(0,model.getLength())
+        Assert.assertEquals(0,model.getLength());
     }
 
 }
