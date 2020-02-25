f6cad3d9e35188117b6e61faa87425dcceefdda0
KySo4oK <prostorostuk08@gmail.com>
2020-02-25 22:21:22 +0200

    repeat code and settings of project(second video on spring framework playlist)

---
+++ HW10/firstappspringcore/src/main/java/spring/firstApp/TestSpring.java
@@ -0,0 +1,14 @@
+package spring.firstApp;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+        TestBean testBean = context.getBean("TestBean", TestBean.class);
+        System.out.println(testBean.getName());
+        context.close();
+    }
+}
