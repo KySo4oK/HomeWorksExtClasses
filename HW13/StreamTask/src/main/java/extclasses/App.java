f19187e0be29a454cc7bfeda531d7e6a98079ef3
KySo4oK <prostorostuk08@gmail.com>
2020-03-03 16:36:59 +0200

    wrote all expect returning min with index of

--- HW13/StreamTask/src/main/java/extclasses/App.java
+++ HW13/StreamTask/src/main/java/extclasses/App.java
@@ -1,13 +1,54 @@
 package extclasses;
 
+import java.util.IntSummaryStatistics;
+import java.util.OptionalDouble;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+
+    }
+
+    public double getAverageWithAverageFunc(int[] ints) {
+        return IntStream.of(ints)
+                .average()
+                .getAsDouble();
+    }
+
+    public double getAverageWithoutAverageFunc(int[] ints) {
+        IntSummaryStatistics intSummaryStatistics =
+                IntStream
+                        .of(ints)
+                        .summaryStatistics();
+        return (double) (intSummaryStatistics.getSum()/intSummaryStatistics.getCount());
+    }
+
+    public int[] getMinValueWithIndex(int[] ints) {
+       return null;
+    }
+
+    public long countEqualsZero(int[] ints) {
+        return IntStream
+                .of(ints)
+                .filter(x -> x == 0)
+                .count();
+    }
+
+    public long countMoreThanZero(int[] ints) {
+        return IntStream
+                .of(ints)
+                .filter(x -> x > 0)
+                .count();
+    }
+
+    public int[] multiplyWithNumber(int[] ints, final int number) {
+        return IntStream
+                .of(ints)
+                .map(x -> x = x * number)
+                .toArray();
     }
 }
--- HW13/StreamTask/src/test/java/extclasses/AppTest.java
+++ HW13/StreamTask/src/test/java/extclasses/AppTest.java
@@ -1,20 +1,72 @@
 package extclasses;
 
+import static junit.framework.TestCase.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
     }
+
+    @Test
+    public void testGetAverageWithAverageFunc() {
+        App app = new App();
+        double result = app.getAverageWithAverageFunc(new int[]{3, 4, 5});
+        assertEquals(result, 4.0);
+    }
+
+    @Test
+    public void testGetAverageWithoutAverageFunc() {
+        App app = new App();
+        double result = app.getAverageWithoutAverageFunc(new int[]{3, 4, 5});
+        assertEquals(result, 4.0);
+    }
+
+    @Ignore
+    @Test
+    public void testGetMinValueWithIndex() {
+        App app = new App();
+        int[] result = app.getMinValueWithIndex(new int[]{3, 4, 5});
+        assertTrue((result[0] == 0) && (result[1] == 3));
+    }
+
+    @Test
+    public void testCountEqualsZero() {
+        App app = new App();
+        long result = app.countEqualsZero(new int[]{0, 0, 0, 0, 3, 0, 4, 5});
+        assertEquals(result, 5);
+    }
+
+    @Test
+    public void testCountMoreThanZero() {
+        App app = new App();
+        long result = app.countMoreThanZero(new int[]{0, 0, 0, 0, 3, 0, 4, 5});
+        assertEquals(result, 3);
+    }
+
+    @Test
+    public void testMultiplyWithNumber() {
+        App app = new App();
+        int[] ints = {0,0,3,4,4,4,5,6,7};
+        int number = 123;
+        int[] result = app.multiplyWithNumber(ints, number);
+        for (int i = 0; i < result.length; i++) {
+            if (result[i] != ints[i]*number) {
+                Assert.fail();
+            }
+        }
+    }
+
+
 }
