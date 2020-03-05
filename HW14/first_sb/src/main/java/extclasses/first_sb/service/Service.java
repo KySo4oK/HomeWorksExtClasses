145ee6307c7402c56fdb6463697d6633ac1638ac
KySo4oK <prostorostuk08@gmail.com>
2020-03-05 18:02:41 +0200

    add changing locale

--- HW14/first_sb/src/main/java/extclasses/first_sb/service/Service.java
+++ HW14/first_sb/src/main/java/extclasses/first_sb/service/Service.java
@@ -1,9 +1,27 @@
 package extclasses.first_sb.service;
 
+import java.util.Enumeration;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 @org.springframework.stereotype.Service
 public class Service {
+    private static final String BUNDLE_NAME = "messages";
+    private static final String ENG = "en";
+    private static final String MAIN_MESSAGE = "main.message";
+    private static ResourceBundle resourceBundle;
 
     public void changeLocale(String lang) {
+        if (lang.equals(ENG)) {
+            resourceBundle =
+                    ResourceBundle.getBundle(BUNDLE_NAME, new Locale(ENG));
+        } else {
+            resourceBundle =
+                    ResourceBundle.getBundle(BUNDLE_NAME, new Locale("UA","ua"));
+        }
+    }
 
+    public void printWithI18n(){
+        resourceBundle.getString(MAIN_MESSAGE);
     }
 }
