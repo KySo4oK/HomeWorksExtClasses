b65df92406d7c5e4f24234f066eb798b56d1d81a
KySo4oK <prostorostuk08@gmail.com>
2020-03-05 16:05:50 +0200

    add get method and fix post

--- HW14/first_sb/src/main/java/extclasses/first_sb/controller/PageController.java
+++ HW14/first_sb/src/main/java/extclasses/first_sb/controller/PageController.java
@@ -1,12 +1,20 @@
 package extclasses.first_sb.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
 
 @Controller
 public class PageController {
     @PostMapping("/")
-    public void changeLocale(String locale) {
-        System.out.println(locale);
+    public String changeLocale(MyLocale myLocale) {
+        System.out.println(myLocale.getLang());
+        return "index.html";
+    }
+
+    @GetMapping("/")
+    public String sendMainPage() {
+        return "index.html";
     }
 }
