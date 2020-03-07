226c101f0704e6aee0f581c49e7de9767bd13e7d
KySo4oK <prostorostuk08@gmail.com>
2020-03-07 15:47:19 +0200

    create project and add packages

---
+++ HW15/sb_with_spring_security/src/main/java/extclasses/sb_with_spring_security/SbWithSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package extclasses.sb_with_spring_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbWithSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbWithSpringSecurityApplication.class, args);
+    }
+
+}
