aa02e3b71ee1af8a2fbc10f0075ca6b175629a20
KySo4oK <prostorostuk08@gmail.com>
2020-03-13 16:54:33 +0200

    add Iterator

---
+++ Code_From_Video_Patterns/src/main/java/com/company/IteratorApp.java
@@ -0,0 +1,44 @@
+package com.company;
+
+public class IteratorApp {
+    public static void main(String[] args) {
+        Tasks tasks = new Tasks();
+        Iterator it = tasks.getIterator();
+        while (it.hasNext()) {
+            System.out.println((String) it.next());
+        }
+    }
+}
+
+interface Iterator {
+    boolean hasNext();
+
+    Object next();
+}
+
+interface Container {
+    Iterator getIterator();
+}
+
+class Tasks implements Container {
+    String[] tasks = {"Build house", "Have a son", "Grow a tree"};
+
+    @Override
+    public Iterator getIterator() {
+        return new TaskIterator();
+    }
+
+    private class TaskIterator implements Iterator {
+        int index = 0;
+
+        @Override
+        public boolean hasNext() {
+            return index < tasks.length;
+        }
+
+        @Override
+        public Object next() {
+            return tasks[index++];
+        }
+    }
+}
