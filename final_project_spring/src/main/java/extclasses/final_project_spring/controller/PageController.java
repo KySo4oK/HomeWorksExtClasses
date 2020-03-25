2dff017e13cb0ccd34fe8f413bd20bb3df415a60
KySo4oK <prostorostuk08@gmail.com>
2020-03-25 23:48:57 +0200

    edit registration page and now it doesnt work

--- final_project_spring/src/main/java/extclasses/final_project_spring/controller/PageController.java
+++ final_project_spring/src/main/java/extclasses/final_project_spring/controller/PageController.java
@@ -1,17 +1,12 @@
 package extclasses.final_project_spring.controller;
 
-import extclasses.final_project_spring.dto.OrderDTO;
 import extclasses.final_project_spring.dto.UserDTO;
 import extclasses.final_project_spring.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.Set;
 
 
 @Controller
@@ -36,8 +31,8 @@ public class PageController {
     }
 
     @PostMapping("/reg")
-    public String getNewUser(UserDTO userDTO) {
+    public @ResponseBody String getNewUser(UserDTO userDTO) {
         userService.setNewUser(userDTO);
-        return "redirect:/";
+        return "Congratulation";
     }
 }
