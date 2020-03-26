0293ac60567775b2ea157a4b20ca1ff8b55f7cdd
KySo4oK <prostorostuk08@gmail.com>
2020-03-26 12:46:34 +0200

    make registration work

--- final_project_spring/src/main/java/extclasses/final_project_spring/controller/PageController.java
+++ final_project_spring/src/main/java/extclasses/final_project_spring/controller/PageController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
@@ -31,7 +32,8 @@ public class PageController {
     }
 
     @PostMapping("/reg")
-    public @ResponseBody String getNewUser(UserDTO userDTO) {
+    public @ResponseBody String getNewUser(@RequestBody UserDTO userDTO) {
+        System.out.println(userDTO);
         userService.setNewUser(userDTO);
         return "Congratulation";
     }
