7fa6dc87b72380d2d0c945b0c7e60b1482139fc4
KySo4oK <prostorostuk08@gmail.com>
2020-03-29 15:51:50 +0300

    add header for main page to all roles

--- final_project_spring/src/main/java/extclasses/final_project_spring/controller/PageController.java
+++ final_project_spring/src/main/java/extclasses/final_project_spring/controller/PageController.java
@@ -3,11 +3,15 @@ package extclasses.final_project_spring.controller;
 import extclasses.final_project_spring.dto.UserDTO;
 import extclasses.final_project_spring.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 
 @Controller
@@ -32,9 +36,19 @@ public class PageController {
     }
 
     @PostMapping("/reg")
-    public @ResponseBody String getNewUser(@RequestBody UserDTO userDTO) {
+    public @ResponseBody
+    String getNewUser(@RequestBody UserDTO userDTO) {
         System.out.println(userDTO);
         userService.setNewUser(userDTO);
         return "Congratulation";
+    }
+
+    @GetMapping("/authorities")
+    public @ResponseBody
+    int getAuthorities(Authentication authentication) {
+        if (authentication == null) return 3;
+        System.out.println(authentication.getAuthorities());
+        String authorities = authentication.getAuthorities().toString();
+        return authorities.contains("ADMIN") ? 1 : 2;
     }
 }
