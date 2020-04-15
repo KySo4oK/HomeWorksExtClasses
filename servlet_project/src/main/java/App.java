a6623133649b8e1164ac3ed7dabd2e0ed70ce88b
KySo4oK <prostorostuk08@gmail.com>
2020-04-15 20:57:31 +0300

    create new servlet project

--- servlet_project/src/main/java/App.java
+++ servlet_project/src/main/java/App.java
@@ -1,12 +1,23 @@
-import controller.RegisterServlet;
+import model.dao.impl.ConnectionPoolHolder;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class App {
     private static final org.apache.logging.log4j.Logger log
             = org.apache.logging.log4j.LogManager.getLogger(App.class);
     public static void main(String[] args) throws SQLException {
-        log.info("sssss");
+        Connection con =
+                ConnectionPoolHolder.getDataSource().
+                        getConnection();
+
+        Statement query = con.createStatement();
+        ResultSet rs = query.executeQuery("SELECT * FROM users");
+        while (rs.next()) {
+            System.out.println(rs.getString("username"));
+        }
 
 
 //
