2ab7d25be39a915424b448b89dbaac6947bb6bdb
KySo4oK <prostorostuk08@gmail.com>
2020-04-16 16:19:20 +0300

    add successful logout

--- servlet_project/src/main/java/controller/LogoutServlet.java
+++ servlet_project/src/main/java/controller/LogoutServlet.java
@@ -7,11 +7,12 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class LogoutServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getSession().setAttribute("userType", "guest");
     }
 }
