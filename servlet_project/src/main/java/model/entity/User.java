382778a29f4cc06ba5376ea8d1df0cf46b22da5f
KySo4oK <prostorostuk08@gmail.com>
2020-04-19 12:43:05 +0300

    add role

--- servlet_project/src/main/java/model/entity/User.java
+++ servlet_project/src/main/java/model/entity/User.java
@@ -4,13 +4,27 @@ public class User {
     private Integer id;
     private String username;
     private String password;
+    public enum ROLE {
+        USER, ADMIN, UNKNOWN
+    }
+
+    private ROLE role;
 
     public User(String username, String password) {
         this.username = username;
         this.password = password;
+        this.role = ROLE.USER;
     }
 
     public User() {
+    }
+
+    public ROLE getRole() {
+        return role;
+    }
+
+    public void setRole(ROLE role) {
+        this.role = role;
     }
 
     public Integer getId() {
